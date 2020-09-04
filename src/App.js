6cbc6e6befffbac516cf4ae149d68d3bea29a5b2
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-09-04 01:12:34 -0400

    first pass complete

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            data: todosData,
-        }
+            data: todosData
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.data.map(item => {
+                if (id === item.id) {
+                    return {
+                        ...item,
+                        completed: !item.completed
+                    }
+                }
+                return item;
+            })
+            return {
+                data: updatedTodos
+            }
+        })
     }
     
     render() {
         const todosArray = this.state.data.map(item => {
-            return <TodoItem key={item.id} todo={item}/>
+            return <TodoItem key={item.id} todo={item} handleChange={this.handleChange}/>
         })
     
         return (
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const TodoItem = (props) => {
+    const completeStyle = {
+        textDecoration: 'line-through'
+    }
     return (
         <div className="todo">
-            <input type="checkbox" checked={props.todo.completed}/>
-            <p>{props.todo.text}</p>
+            <input 
+                type="checkbox" 
+                checked={props.todo.completed} 
+                onChange={() => props.handleChange(props.todo.id)}/>
+            <p style={props.todo.completed ? completeStyle : null}>{props.todo.text}</p>
         </div>
     )
 }
